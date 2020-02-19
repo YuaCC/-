9b1088b3e5d2c0f727599f57b4141ee6ff8b3492
YuaCC <1095192336@qq.com>
2020-02-19 14:38:25 +0800

    Update RemoveHuiche.py

--- RemoveHuiche.py
+++ RemoveHuiche.py
@@ -2,15 +2,14 @@ import time
 import sys
 import os
 import re
-sys.path.append(os.path.abspath("SO_site-packages"))
-import pyperclip 
+import pyperclip
 new_value = ""
 old_value = ""
-reg=re.compile(r"[\r\n]+")
+reg=re.compile(r"\s+")
 while True:
     new_value = pyperclip.paste()
     if new_value != old_value:
-        new_value=reg.sub(" ", new_value)
+        new_value=reg.sub("", new_value)
         pyperclip.copy(new_value)
         print(new_value)
         old_value=new_value
